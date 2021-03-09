d3483e1bf9fdceba7be2c432b4307608d7f6fea4
NedVill <liamholwett@gmail.com>
2021-03-09 10:04:11 +0300

    fix: fix responsive

---
+++ build/precache-manifest.31012fd3385929f98e98df9e3b8f8fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8876412c4c192b320dfcf8354e4e277",
+    "url": "./index.html"
+  },
+  {
+    "revision": "462c7414675752392d98",
+    "url": "./static/css/main.45ca9044.chunk.css"
+  },
+  {
+    "revision": "85c9e5226050da5ee3d4",
+    "url": "./static/js/2.3e04dd95.chunk.js"
+  },
+  {
+    "revision": "462c7414675752392d98",
+    "url": "./static/js/main.f75548ce.chunk.js"
+  },
+  {
+    "revision": "5b75a27f26e052dcd82a",
+    "url": "./static/js/runtime-main.10492562.js"
+  },
+  {
+    "revision": "cbad05ca005311fc3faa253a801435e1",
+    "url": "./static/media/paper-plane-solid.cbad05ca.svg"
+  }
+]);
